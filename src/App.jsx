f46fcbc9703ff13e8b2a4d359ae575ae12ff8b63
Garett-Lubecki <garett934@gmail.com>
2023-08-10 18:47:46 -0400

    add garetts log in stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,33 @@ import React, { useEffect, useState } from "react";
 import AccountServices from "./Components/LoginPage/AccountServices";
 import Header from "./Components/AfterLogin/Header";
 import { AccountProvider } from "./Components/Context/AccountServicesContext";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom' 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <>
-      <AccountProvider>
-        <AccountServices />
-      </AccountProvider>
-
-      <Header />
-    </>
+    <Router>
+      <Routes>
+      <Route exact path="/" element={
+              <AccountProvider>
+              <AccountServices />
+            </AccountProvider>
+      }>
+      </Route>
+      <Route exact path="/manager" element={
+        <Header />
+        //Rest of student stuff goes here
+      }>
+      </Route>
+            <Route exact path="/manager" element={
+        <Header />
+        //Rest of student stuff goes here
+      }>
+      </Route>
+      </Routes>
+    </Router>
     </>
   )
 }
