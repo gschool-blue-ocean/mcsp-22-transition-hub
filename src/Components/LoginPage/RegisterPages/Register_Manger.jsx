ff55ab6d55a9ef730cc10091f2098769e29d5848
yasminaguilarx <yaguilar2001@gmail.com>
2023-08-11 16:34:56 -0500

    pushing bc i was told to by git

--- src/Components/LoginPage/RegisterPages/Register_Manger.jsx
+++ src/Components/LoginPage/RegisterPages/Register_Manger.jsx
@@ -1,79 +1,125 @@
-import React from "react"
-import './Register_Manager.css'
+import React from "react";
+import "./Register_Manager.css";
 import { useState } from "react";
 
 const Register_Manager = () => {
-    const [formData, setFormData] = useState({
-        username: "",
-        password: "",
-        cohort: "",
-        firstName: "",
-        lastName: "",
-        email: "",
-        role: "manager"
-    });
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      };
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+    cohort: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    role: "manager",
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-return (
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { email, userName, password };
+      const res = await fetch("http://localhost:4500/authorization/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }); // may have to change route, unsure at this time
+      const parseRes = await response.json();
+
+      if (parseRes.jwtToken) {
+        localStorage.setItem("token", parseRes.jwtToken);
+        setAuth(true);
+        TransformStream.success("Register Successful");
+      } else {
+        setAuth(false);
+        toast.error(parseRes);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
     <>
-    <div className="Register_Manager_Title">
-        Welcome, new Career Services Manager!
-    </div>
-        <form className="Register_Manager_Form">
-            <div className="Register_Manager_Form_Input_Container">
-                <label>UserName</label>
-                <input type='text' placeholder=''
-                name="username"
-                onChange={handleChange}
-                value={formData.username}></input>
-            </div>
-            <div className="Register_Manager_Form_Input_Container">
-                <label>Password</label>
-                <input type='password' placeholder=''
-                name="password"
-                onChange={handleChange}
-                value={formData.password}></input>
-            </div>
-            <div className="Register_Manager_Form_Input_Container">
-                <label>Cohort</label>
-                <input type='text' placeholder=''
-                name="cohort"
-                onChange={handleChange}
-                value={formData.cohort}></input>
-            </div>
-            <div className="Register_Manager_Form_Input_Name">
-            <div className="Register_Manager_Form_Input_Container">
-                <label>First Name</label>
-                <input type='text' placeholder=''
-                name="firstName"
-                onChange={handleChange}
-                value={formData.firstName}></input>
-            </div>
-            <div className="Register_Manager_Form_Input_Container">
-                <label>Last Name</label>
-                <input type='text' placeholder=''
-                name="lastName"
-                onChange={handleChange}
-                value={formData.lastName}></input>
-            </div>
-            </div>
+      <div className='Register_Manager_Title'>
+        Welcome, New Career Services Manager!
+      </div>
+      <form className='Register_Manager_Form'>
+        <div className='Register_Manager_Form_Input_Container'>
+          <label>UserName</label>
+          <input
+            type='text'
+            placeholder=''
+            name='username'
+            onChange={handleChange}
+            value={formData.userName}
+          ></input>
+        </div>
+        <div className='Register_Manager_Form_Input_Container'>
+          <label>Password</label>
+          <input
+            type='password'
+            placeholder=''
+            name='password'
+            onChange={handleChange}
+            value={formData.password}
+          ></input>
+        </div>
+        <div className='Register_Manager_Form_Input_Container'>
+          <label>Cohort</label>
+          <input
+            type='text'
+            placeholder=''
+            name='cohort'
+            onChange={(e) => handleChange(e)}
+            value={formData.cohort}
+          ></input>
+        </div>
+        <div className='Register_Manager_Form_Input_Name'>
+          <div className='Register_Manager_Form_Input_Container'>
+            <label>First Name</label>
+            <input
+              type='text'
+              placeholder=''
+              name='firstName'
+              onChange={(e) => handleChange(e)}
+              value={formData.firstName}
+            ></input>
+          </div>
+          <div className='Register_Manager_Form_Input_Container'>
+            <label>Last Name</label>
+            <input
+              type='text'
+              placeholder=''
+              name='lastName'
+              onChange={(e) => handleChange(e)}
+              value={formData.lastName}
+            ></input>
+          </div>
+        </div>
 
-            <div className="Register_Manager_Form_Input_Container">
-                <label>Email</label>
-                <input type='email' placeholder=''
-                name="email"
-                onChange={handleChange}
-                value={formData.email}></input>
-            </div>
-            <button className="Register_Button" id="Register_Manager_Button">Register</button>
-        </form>
+        <div className='Register_Manager_Form_Input_Container'>
+          <label>Email</label>
+          <input
+            type='email'
+            placeholder=''
+            name='email'
+            onChange={(e) => handleChange(e)}
+            value={formData.email}
+          ></input>
+        </div>
+        <button className='Register_Button' id='Register_Manager_Button'>
+          Register
+        </button>
+      </form>
+      <Link to='/login'>Login</Link>
     </>
-)
+  );
+};
 
-}
-
-export default Register_Manager
+export default Register_Manager;
--- src/Components/LoginPage/SignOn.jsx
+++ src/Components/LoginPage/SignOn.jsx
@@ -1,47 +1,60 @@
 import React from "react";
 import "./SignOn.css";
-import { useContext, useState} from 'react'
-import AccountContext from '../Context/AccountServicesContext'
+import { useContext, useState } from "react";
+//import { Link, Redirect } from "react-router-dom";
+import AccountContext from "../Context/AccountServicesContext";
 
-const SignOn = () => {
-    const {setCurrentService, accountServices} = useContext(AccountContext)
-    const [formData, setFormData] = useState({
-        username: "",
-        password: ""
-      });
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      };
+const SignOn = ({ setAuth }) => {
+  const { setCurrentService, accountServices } = useContext(AccountContext);
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    return (
-        <div className="logOnBG">
-            <div className="logOnContainer">
-                <div className="logOn_Title">
-                    LogIn
-                </div>
-                <div className="login-form">
-                    <div className="login_Input">
-                        <label>User Name</label>
-                        <input type="text" name="username" 
-                        onChange={handleChange}
-                        value={formData.username}/>
-                    </div>
-                    <div className="login_Input">
-                        <label>Password</label>
-                        <input type="password" name="password" 
-                        onChange={handleChange}
-                        value={formData.password}
-                        />
-                    </div>
-                    <button className ="SignOn_Buttons" id="signOn_Submit" type="submit">Log On</button>
-                </div>
-                <div className="signOn_Register_Container">
-                    <button className ="SignOn_Buttons" id="signOn_Register" type="button" onClick={() => setCurrentService(accountServices[1])}>Register</button>
-                </div>
-            </div>
+  return (
+    <div className='logOnBG'>
+      <div className='logOnContainer'>
+        <div className='logOn_Title'>LogIn</div>
+        <div className='login-form'>
+          <div className='login_Input'>
+            <label>User Name</label>
+            <input
+              type='text'
+              name='username'
+              onChange={handleChange}
+              value={formData.userName}
+            />
+          </div>
+          <div className='login_Input'>
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              onChange={handleChange}
+              value={formData.password}
+            />
+          </div>
+          <button className='SignOn_Buttons' id='signOn_Submit' type='submit'>
+            Log On
+          </button>
         </div>
-    );
+        <div className='signOn_Register_Container'>
+          <button
+            className='SignOn_Buttons'
+            id='signOn_Register'
+            type='button'
+            onClick={() => setCurrentService(accountServices[1])}
+          >
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignOn;
