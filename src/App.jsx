588b75518d2041a5438757b4c07022f21a9e4b06
Garett-Lubecki <garett934@gmail.com>
2023-08-16 18:37:29 -0400

    add background to sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { AuthProvider } from "../Authorization/utils/AuthContext";
+import { AuthProvider } from "./Components/Context/AuthContext";
 import LogInPage from "./Components/MainComponents/LogInPage";
 import Manager from "./Components/MainComponents/Manager";
 import Student from "./Components/MainComponents/Student";
