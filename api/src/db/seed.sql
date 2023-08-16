1b202a131d6ea7aec4c572b488a572ae4b8b9007
Garett Lubecki <127802687+Garett-Lubecki@users.noreply.github.com>
2023-08-16 13:39:20 -0400

    Delete api directory

--- api/src/db/seed.sql
+++
@@ -1,6 +0,0 @@
--- INSERT INTO comments(managersId, tasksId, description) VALUES('Do the dishes');
-INSERT INTO tasks(studentsId, taskName, taskDescription, dueDate, apptDate) VALUES('Walk the dog');
-INSERT INTO students(usersId, cohortsId, ets, branch, clearanceType) VALUES('Sweep the floor');
-INSERT INTO managers(usersId, cohortsId) VALUES('Do your homework');
-INSERT INTO users(cohortsId, userName, password, firstName, lastName, email, role) VALUES('Beat Elden Ring');
-INSERT INTO cohorts(cohortsName, startDate, endDate) VALUES('Beat Elden Ring');
