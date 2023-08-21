2bbdf52cdc4b90b060687d8cc5d986bd0c4cd56a
Dorion Boesch <dorion.boesch@gmail.com>
2023-08-21 10:04:44 -0500

    changed the url to render

--- src/Components/Context/UrlContext.jsx
+++ src/Components/Context/UrlContext.jsx
@@ -1,16 +1,19 @@
-import {useState, createContext} from 'react'
+import { useState, createContext } from "react";
 
-const UrlContext = createContext()
+const UrlContext = createContext();
 
-export const UrlProvider = ({children}) => {
-    const [url, setUrl] = useState('http://localhost:8000')
+export const UrlProvider = ({ children }) => {
+  const [url, setUrl] = useState("https://transition-hub.onrender.com/");
 
-    
-    return <UrlContext.Provider value={{
-        url
-    }}>
-        {children}
-        </UrlContext.Provider>
-}
+  return (
+    <UrlContext.Provider
+      value={{
+        url,
+      }}
+    >
+      {children}
+    </UrlContext.Provider>
+  );
+};
 
-export default UrlContext
+export default UrlContext;
