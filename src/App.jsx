062726015326ed95420d89bab5792c6c3d0aa607
Garett-Lubecki <garett934@gmail.com>
2023-08-23 18:07:49 -0400

    trying hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   useParams
@@ -10,40 +10,32 @@ import Manager from "./Components/MainComponents/Manager";
 import Student from "./Components/MainComponents/Student";  
 import StudentInManager from "./Components/AfterLogin/ManagerViewOnly/StudentInManager";
 
-
 function App() {
   const isAuthenticated = true; // not used
   const isManager = true; //not used 
   const isStudent = true; //not used 
-  const { id } = useParams()
+  const { studentIdentification } = useParams();
+  
   return (
     <StudentProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<LogInPage />}></Route>
-              <Route
-                path="/manager"
-                element={
-                  isAuthenticated && isManager ? <Manager /> : <LogInPage />
-                }
-              ></Route>
-              <Route
-                path="/student/:studentIdentification"
-                element={
-                  isAuthenticated && isStudent ? <Student /> : <LogInPage />
-                }
-              ></Route>
-              <Route
-                path="manager/:studentIdentification"
-                element={
-                   <StudentInManager />
-                }
-              ></Route>
-            </Routes>
-          </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LogInPage />} />
+          <Route
+            path="/manager"
+            element={isAuthenticated && isManager ? <Manager /> : <LogInPage />}
+          />
+          <Route
+            path="/student/:studentIdentification"
+            element={isAuthenticated && isStudent ? <Student /> : <LogInPage />}
+          />
+          <Route
+            path="/manager/:studentIdentification"
+            element={<StudentInManager />}
+          />
+        </Routes>
+      </Router>
     </StudentProvider>
-
-
   );
 }
 
