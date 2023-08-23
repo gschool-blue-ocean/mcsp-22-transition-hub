1e4b5c663ff9806c8020595b30b5a1ffb9a42b80
Garett-Lubecki <garett934@gmail.com>
2023-08-23 17:45:21 -0400

    correct imports with server route

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-//import { studentRouter } from "../Authorization/routes/student.js";
-// import pg from "pg";
 import pkg from "pg";
 import cors from "cors";
 import pool from "./DB/db.js";
@@ -12,18 +10,18 @@ import { manageRouter } from "./Authorization/routes/manager.js";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
-// const staticPath = path.join(__dirname, "MCSP-22-TRANSITION-HUB");
+
 app.use(cors());
 app.use(express.static("dist"));
-// app.use(express.static(staticPath));
-// const { Pool } = pkg;
-
-// const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-
 app.use(express.json());
 
 app.use("/api/auth", userRouter);
 app.use("/api/manager", manageRouter);
+
+// Serve the React app's index.html for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 // -------------- SERVER ROUTES FOR TASKS --------------------
 
