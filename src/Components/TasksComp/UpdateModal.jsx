79e23e24a36a65e7c20282a039dc392fc7578f61
Dorion Boesch <dorion.boesch@gmail.com>
2023-08-24 16:21:17 -0500

    final* css push

--- src/Components/TasksComp/UpdateModal.jsx
+++ src/Components/TasksComp/UpdateModal.jsx
@@ -1,12 +1,9 @@
-
-import React, { useState, useEffect } from 'react';
-import moment from 'moment';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import "./UpdateModal.css"
-
-
-
+import React, { useState, useEffect, useContext } from "react";
+import moment from "moment";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./UpdateModal.css";
+import UrlContext from "../Context/UrlContext";
 
 // toast.configure();
 
@@ -51,23 +48,19 @@ const UpdateModal = ({ taskId, initialData, closeModal, onTaskUpdate }) => {
       });
 
       if (response.ok) {
-
-        toast.success('Task updated successfully');
-        const updatedTask = await response.json(); 
+        toast.success("Task updated successfully");
+        const updatedTask = await response.json();
 
         onTaskUpdate(updatedTask);
         closeModal();
       } else {
-
-        toast.error('Failed to update task');
+        toast.error("Failed to update task");
       }
     } catch (err) {
-      toast.error('Failed to update task');
+      toast.error("Failed to update task");
 
       console.error(err);
     }
-
-    
   };
 
   return (
